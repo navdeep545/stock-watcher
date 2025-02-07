8e1980ca3e20ec1251e640e784092a917390654f
Navdeep Jajoria <rk6578355@gmail.com>
2025-02-07 15:01:48 +0530

    eslint error

--- src/app/providers/AuthProvider.tsx
+++ src/app/providers/AuthProvider.tsx
@@ -6,6 +6,7 @@ export default function AuthProvider({
   children,
   session,
 }: {
+  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
   children: React.ReactNode;
   session: any;
 }) {
